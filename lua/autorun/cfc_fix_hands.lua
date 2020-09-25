ffc8486fb87d2905059d64f1a71f06858b2259b3
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-24 21:39:53 -0400

    Fix hands

---
+++ lua/autorun/cfc_fix_hands.lua
@@ -0,0 +1,26 @@
+local function fixHands()
+    local hands = scripted_ents.GetStored("gmod_hands").t
+
+    hands.Initialize = function(self)
+        if CLIENT then
+            if self:GetOwner() == LocalPlayer() then
+                self.viewHookID = "HandsViewModelChanged_" .. self:EntIndex()
+
+                self:CallOnRemove( "RemoveViewModelChangedHook", function()
+                    hook.Remove( "OnViewModelChanged", self.viewHookID )
+                end )
+
+                hook.Add( "OnViewModelChanged", self.viewHookID, function(...)
+                    self:ViewModelChanged( ... )
+                end )
+            end
+        end
+
+        self:SetNotSolid( true )
+        self:DrawShadow( false )
+        self:SetTransmitWithParent( true ) -- Transmit only when the viewmodel does!
+    end
+end
+
+
+hook.Add( "Initialize", "CFC_FixHands", fixHands )
